e8aa49cf7c13f7d240ade7cf8fb750d5695c5654
Julia Ovcharenko <samsung2100yuliyu@gmail.com>
2025-12-19 10:07:49 +0200

    for merge

--- src/category/category.router.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { CategoryController } from './category.controller';
-export const CategoryRouter = Router();
-
-
-CategoryRouter.get('/', CategoryController.getAllCategories)
-
